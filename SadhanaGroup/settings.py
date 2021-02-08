0a9c07906f9248aa3074e8779cfbaecf77c0ed8a
Vivek <kingdragon46@gmail.com>
2021-02-08 23:42:02 +0530

    Commit2

--- SadhanaGroup/settings.py
+++ SadhanaGroup/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,3 +127,5 @@ MEDIA_URL = '/images/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+django_heroku.settings(locals())
